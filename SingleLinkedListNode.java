b4af06d0236f2433628e32e5ed2489c7693c744d
Allwin Savari Raj <allwin.amazon@gmail.com>
2015-10-19 19:09:56 +0530

    SingleLinkedListNode Added

---
+++ SingleLinkedListNode.java
@@ -0,0 +1,24 @@
+//package LinkedList.singleLL;
+
+public class SingleLinkedListNode {
+
+	private int data;
+	private SingleLinkedListNode next;
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public SingleLinkedListNode getNext() {
+		return next;
+	}
+
+	public void setNext(SingleLinkedListNode next) {
+		this.next = next;
+	}
+
+}
